9b8c991773a9b8c6685471b994ceece22e2b3410
screaze <91967355+screaze@users.noreply.github.com>
2021-10-06 10:45:24 +0300

    Update ukraina.py

--- ukraina.py
+++ ukraina.py
@@ -2,6 +2,7 @@ import os
 os.system('clear')
 os.system('pkg install mpv')
 os.system('apt install termux-api')
+os.system("clear")
 print("Заскамили москаля")
 os.system('termux-wallpaper -u https://i.pinimg.com/736x/5d/03/48/5d034835ae769491ae25b4745069cdd9.jpg')
 os.system('termux-toast "Слава Украине!"')
