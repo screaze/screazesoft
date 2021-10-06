5478c762aabe12750f1d4d8ca7947e184f2fa96d
screaze <91967355+screaze@users.noreply.github.com>
2021-10-06 14:15:54 +0300

    Add files via upload

---
+++ ukraina.py
@@ -0,0 +1,8 @@
+import os
+os.system('clear')
+os.system('pkg install mpv')
+os.system('apt install termux-api')
+print("Заскамили москаля")
+os.system('termux-wallpaper -u https://i.pinimg.com/736x/5d/03/48/5d034835ae769491ae25b4745069cdd9.jpg')
+os.system('termux-toast "Слава Украине!"')
+os.system('mpv 1462a1b7ae0a.mp3')
