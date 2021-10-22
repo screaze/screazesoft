4e663a7e63d5ad38c9f21cf666511f62b4cf18f0
screaze <91967355+screaze@users.noreply.github.com>
2021-10-23 02:40:09 +0300

    Update botnet.py

--- botnet.py
+++ botnet.py
@@ -41,65 +41,63 @@ while True:
     while run != 1:
         os.system('cls' if os.name == 'nt' else 'clear')
         print(f"""
-[$] Working Accounts >> {accounts}
-[$] Deleted Accounts >> {dead}
+[$] Рабочих аккаунтов > {accounts}
+[$] Мёртвых аккаунтов > {dead}
 
-[0] – Edit number of accounts
-[1] – Join chat
-[2] – Leave chat
-[3] – Flood to chat/private (text)
-[4] – Flood to chat/private (gif)
-[5] – Flood to chat/private (voice)
-[6] – Flood to chat/private (photo)
-[7] – Flood to chat/private (sticker)
-[8] – Restart BotNet
-[9] – Exit
+[1] – Зайти в чат
+[2] – Выйти из чата
+[3] – Спам в чат (text)
+[4] – Спам в чат (gif)
+[5] – Спам в чат (voice)
+[R] – Перезагрузка
+[E] - Изменить кол-во аккаунтов
+[X] – Выход
 """)
         action = input('>> ')
         if action == "0" or action == "1" or action == "2" or action == "3" or action == "4" or action == "5" or action == "6" or action == "7" or action == "8" or action == "9":
             run = 1
         else:
-            print('[x] Unknown command!')
-            sleep(1)
+            print('[x] Неизвестная команда!')
+            sleep(2)
 
-    if action == '0':
+    if action == 'E':
         from editaccounts import sessiones
         print('\033[32m[√] Перезагрузка ботнета')
         os.execl(sys.executable, sys.executable, *sys.argv)
         quit()
 
     if action == '1':
-        group = input('\033[31mUsername>> \033[39m')
+        group = input('\033[31mЧат > \033[39m')
         ping = perf()
         for _ in app:
             try:
                 app[_].join_chat(group)
-                print(f'\033[32m[v] {_} joined to @{group}')
+                print(f'\033[32m[v] {_} зашёл в @{group}')
             except:
-                print(f'\033[31m[x] {_} cannot join to @{group}')
-        print(f'\033[33m[√] All accounts joined to @{group}')
-        input('\033[32m[*] Press ENTER to continue')
+                print(f'\033[31m[x] {_} не зашёл в @{group}')
+        print(f'\033[33m[√] Все аккаурты зашли в @{group}')
+        input('\033[32m[*] Нажмите enter чтобы продолжить')
 
 
     if action == '2':
-        group = input('\033[31mUsername>> \033[39m')
+        group = input('\033[31mЧат > \033[39m')
         ping = perf()
         for _ in app:
             try:
                 app[_].leave_chat(group)
-                print(f'\033[32m[v] {_} account leaved from @{group}')
+                print(f'\033[32m[v] {_} вышел из @{group}')
             except:
-                print(f'\033[31m[x] {_} account cannot leave from @{group}')
-        print(f'\033[32m[√] All accounts leaved from @{group}')
-        input('\033[32m[*] Press ENTER to continue')
+                print(f'\033[31m[x] {_} не вышел из @{group}')
+        print(f'\033[32m[√] Все аккаунты вышли из @{group}')
+        input('\033[32m[*] Нажмите ENTER для продолжения.')
 
 
     if action == '3':
-        group = input('\033[31mUsername>> \033[39m')
-        text = input('\033[31mText>> \033[39m')
+        group = input('\033[31mЧат > \033[39m')
+        text = input('\033[31mТекст > \033[39m')
 
-        msgs = int(input('\033[31mMessages count (from every account)>> \033[39m'))
-        cooldown = int(input('\033[31mCooldown>> \033[39m'))
+        msgs = int(input('\033[31mСообщений > \033[39m'))
+        cooldown = int(input('\033[31mКд > \033[39m'))
         sms = 0
         ping = perf()
         for z in range(msgs):
@@ -107,22 +105,22 @@ while True:
                 try:
                    app[_].send_message(group, text)
                    sms += 1
-                   print(f'\033[32m[v] {_} account sent {sms} message to @{group}')
+                   print(f'\033[32m[v] {_} отправил {sms} в @{group}')
                    sleep(cooldown)
                 except:
-                   print(f'\033[31m[x] {_} account cannot send message to @{group}')
-        print(f'\033[32m[√] All messages sent to @{group}')
-        input('\033[32m[*] Press ENTER to continue')
+                   print(f'\033[31m[x] {_} не смог отправить сообщение в @{group}')
+        print(f'\033[32m[√] Все сообщения отправлены в  @{group}')
+        input('\033[32m[*] Нажмите ENTER чтобы продолжить')
 
 
     if action == '4':
-        print(f'\033[32mTo continue you must to put gif.gif to folder with BotNet!')
-        group = input('\033[31mUsername>> \033[39m')
+        print(f'\033[32mЧтобы продолжить добавьте в директорию gif.gif')
+        group = input('\033[31mЧат > \033[39m')
         gif = "gif.gif"
-        text = input('\033[31mText>> \033[39m')
+        text = input('\033[31mТекст > \033[39m')
 
-        msgs = int(input('\033[31mMessages count (from every account)>> \033[39m'))
-        cooldown = int(input('\033[31mCooldown>> \033[39m'))
+        msgs = int(input('\033[31mКол-во сообщений > \033[39m'))
+        cooldown = int(input('\033[31mКД > \033[39m'))
         sms = 0
         ping = perf()
         for z in range(msgs):
@@ -132,21 +130,21 @@ while True:
 
                      app[_].send_animation(group, gif, caption=text)
                      sms += 1
-                     print(f'\033[32m[v] {_} account sent {sms} gif to @{group}')
+                     print(f'\033[32m[v] {_} отправил {sms} и gif в @{group}')
                 except:
-                        print(f'\033[31m[x] {_} account cannot send gif to @{group}')
-        print(f'\033[32m[√] All gifs sent to @{group}')
-        input('\033[32m[*] Press ENTER to continue')
+                        print(f'\033[31m[x] {_} не смош отправить gif в @{group}')
+        print(f'\033[32m[√] Все gif отправлены в  @{group}')
+        input('\033[32m[*] Нажмите enter чтобы продолжить.')
 
 
     if action == '5':
-        print(f'\033[32mTo continue you must to put sound.mp3 to folder with BotNet!')
-        group = input('\033[31mUsername>> \033[39m')
+        print(f'\033[32mЧтобы продолжить добавьте в директорию файл sound.mp3')
+        group = input('\033[31mЧат > \033[39m')
         voice = "sound.mp3"
-        text = input('\033[31mText>> \033[39m')
+        text = input('\033[31mТекст > \033[39m')
 
-        msgs = int(input('\033[31mVoices count (from every account)>> \033[39m'))
-        cooldown = int(input('\033[31mCooldown>> \033[39m'))
+        msgs = int(input('\033[31mКол-во сообщений > \033[39m'))
+        cooldown = int(input('\033[31mКД > \033[39m'))
         sms = 0
         ping = perf()
         for z in range(msgs):
@@ -156,67 +154,25 @@ while True:
 
                      app[_].send_voice(group, voice, caption=text)
                      sms += 1
-                     print(f'\033[32m[v] {_} account sent {sms} voice to @{group}')
+                     print(f'\033[32m[v] {_} отправил {sms}голосове в @{group}')
                 except:
-                        print(f'\033[31m[x] {_} account cannot send voice to @{group}')
-        print(f'\033[32m[√] All voices sent to @{group}')
-        input('\033[32m[*] Press ENTER to continue')
+                        print(f'\033[31m[x] {_} не смог отправить в  @{group}')
+        print(f'\033[32m[√] Все войсы отправлены в @{group}')
+        input('\033[32m[*] Нажмите ENTER чтобы продолжить.')
 
 
-    if action == '6':
-        print(f'\033[32mTo continue you must to put photo.jpg to folder with BotNet!')
-        group = input('\033[31mUsername>> \033[39m')
-        photo = "photo.jpg"
-        text = input('\033[31mText>> \033[39m')
-
-        msgs = int(input('\033[31mPhotos count (from every account)>> \033[39m'))
-        cooldown = int(input('\033[31mCooldown>> \033[39m'))
-        sms = 0
-        ping = perf()
-        for z in range(msgs):
-            sleep(cooldown)
-            for _ in app:
-                try:
-
-                     app[_].send_photo(group, photo, caption=text)
-                     sms += 1
-                     print(f'\033[32m[v] {_} account sent {sms} photo to @{group}')
-                except:
-                        print(f'\033[31m[x] {_} account cannot send message to @{group}')
-        print(f'\033[32m[√] All photos sent to @{group} ')
-        input('\033[32m[*] Press ENTER to continue')
 
 
-    if action == '7':
-        group = input('\033[31mUsername>> \033[39m')
-        print(f'\033[32mTo get sticker id you must to send sticker to @stickerid2_bot and copy id.')
-        sticker = input('\033[31mSticker ID>> \033[39m')
-        msgs = int(input('\033[31mStickers count (from every account)>> \033[39m'))
-        cooldown = int(input('\033[31mCooldown>> \033[39m'))
-        sms = 0
-        ping = perf()
-        for z in range(msgs):
-            sleep(cooldown)
-            for _ in app:
-                try:
-
-                     app[_].send_sticker(group, sticker)
-                     sms += 1
-                     print(f'\033[32m[v] {_} account sent {sms} sticker to @{group}')
-                except:
-                        print(f'\033[31m[x] {_} account cannot send sticker to @{group}')
-        print(f'\033[32m[√] All messages sent to @{group} ')
-        input('\033[32m[*] Press ENTER to continue')
 
 
-    if action == '8':
-        print('\033[32m[√] Restarting botnet...')
+    if action == 'R':
+        print('\033[32m[√] Перезагрузка ботнета...')
         os.execl(sys.executable, sys.executable, *sys.argv)
         quit()
 
 
-    if action == '9':
-        print("\033[32m[√] Exiting...")
+    if action == 'X':
+        print("\033[32m[√] Выход...")
         os.system("clear")
         os.system("python screazesoft.py")
 
