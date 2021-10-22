9527639a97065ae07dee873a08a349eb152ba018
screaze <91967355+screaze@users.noreply.github.com>
2021-10-23 02:46:46 +0300

    Update screazesoft.py

--- screazesoft.py
+++ screazesoft.py
@@ -15,7 +15,7 @@ slowprint("[5] - Хакинг", 0.1)
 slowprint("[6] - Дединсайд ботнет", 0.1)
 slowprint("[7] - Ботнет для телеграмма", 0.1)
 slowprint("[8] - Дискорд нитро генератор", 0.1)
-slowprint("[9] - Termux-API/Other", 0.1)
+slowprint("[9] - ХэшДетектор/Пробив по нику", 0.1)
 slowprint("[10] - Дискорд крашер.", 0.1)
 slowprint("[X] - Выход", 0.1)
 menu = input("\nВведите пункт из меню: ")
@@ -128,77 +128,14 @@ if menu == "8":
 	
 if menu == "9":
 	os.system("clear")
-	print("[A] - Включить фонарик")
-	print("[B] - Выключить фонарик")
-	print("[C] - Включить Wi-Fi")
-	print("[D] - Выключить Wi-Fi")
-	print("[E] - Поставить флаг Украины на обои.")
 	print("[N/H] Искатель по нику & Определитель хэшей by sans")
 	print("[X] - Назад")
 	
 menu2 = input("Введите пункт из меню: ")
 	
-if menu2 == "A":
-	os.system("clear")
-	print("Фонарик включён.")
-	os.system("termux-torch on")
-	os.system("python screazesoft.py")
-
-if menu2 == "B":
-	os.system("clear")
-	print("Фонарик выключен.")
-	os.system("termux-torch off")
-	os.system("python screazesoft.py")
-	
-if menu2 == "C":
-	os.system("clear")
-	print("Wi-Fi Включён!")
-	os.system("termux-wifi-enable on")
-	os.system("python screazesoft.py")
-	
-if menu2 == "D":
-	os.system("clear")
-	print("Wi-Fi Отключён!")
-	os.system("termux-wifi-enable off")
-	os.system("python screazesoft.py")
-	
-if menu2 == "E":
-	os.system("clear")
-	print("Обои устанавливаются...")
-	os.system("termux-wallpaper -u https://i.pinimg.com/736x/5d/03/48/5d034835ae769491ae25b4745069cdd9.jpg")
-	print("Обои успешно установлены!")
 if menu2 == "X":
     os.system("clear")
     os.system("python screazesoft.py")
-
-if menu2 == "Easter Egg":
-    os.system("clear")
-    print("[1] - Украинцы")
-    print("[2] - Немцы")
-    egg = input("Выберите пункт из меню:  ")
-    if egg == "1":
-        os.system("clear")
-        os.system('pkg install mpv -y')
-        os.system('apt install termux-api -y')
-        print("Заскамили москаля")
-        os.system('termux-wallpaper -u https://i.pinimg.com/736x/5d/03/48/5d034835ae769491ae25b4745069cdd9.jpg')
-        os.system('termux-toast "Слава Украине!"')
-        os.system('mpv 1462a1b7ae0a.mp3')
-        
-        
-        	
-    if egg == "2":
-        os.system("clear")
-        os.system('pkg install mpv -y')
-        os.system('apt install termux-api -y')
-        print("Теперь ты нiмецкiй воiн")
-        os.system('termux-wallpaper -u https://images.app.goo.gl/AAnDtBTcRJpVbrSh9')
-        os.system('termux-toast "Sieg Heil!"')
-        os.system('mpv msi.mp3')
-        
-    else:
-        os.system("clear")
-        os.system("python screazesoft.py")
 	
 if menu2 == "N":
 	os.system("clear")
